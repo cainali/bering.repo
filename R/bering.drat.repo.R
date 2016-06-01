115ea59513f1f389225c1a7cad5fcc962c3eebe7
cainali <cainali@beringresearch.com>
2016-06-01 21:30:02 +0300

    change of script

--- R/bering.drat.repo.R
+++ R/bering.drat.repo.R
@@ -1,19 +1,29 @@
 
-## Call drat package
+# Create the drat directory with the binary dirs
+#bering.repo.dir(drat)
+
+
+## Call drat package and randomForest
+if(!require("randomForest")) {
+    install.packages("randomForest", repos='http://cran.us.r-project.org', type="source")
+    library("randomForest")
+}
 
 if(!require("drat")) {
-    install.packages("drat")
+    install.packages("drat", repos="http://eddelbuettel.github.io/drat")
     library("drat")
 }
 
 
 # Call repo with drat packages 
-drat::addRepo("beringresearch/r-cluster")  ## or just addRepo
+#drat::addRepo("idroz", 'file:///vagrant/drat')  ## or just addRepo
 
 # install packages
-install.packages("bering.ml")     
-install.packages("bering.d3")
-install.packages("bering.ui")
+
+
+install.packages("bering.ml", repos = "file://vagrant/drat", type="source")     
+install.packages("bering.d3", repos = "file://vagrant/drat", type="source")
+install.packages("bering.ui", repos = "file://vagrant/drat", type="source")
 
 # update already installed packages
-update.packages()
+update.packages(ask=FALSE)
